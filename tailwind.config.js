ac686073616308c80952dc745a9a835c0c543f5b
Jesse Gutierrez <jessegutierrez@Jesses-MBP.fios-router.home>
2022-08-06 16:50:43 -0400

    added a cloud and changed the colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'rubik-mono-one': ['"Rubik Mono One"', 'sans-serif'],
+        'poppins': ['"poppins"', 'sans-serif'],
       },
     },
   },
